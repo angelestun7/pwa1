d6fe91e7d089bb9064133e0612fbd19e9b1225d4
angelestun7 <144195125+angelestun7@users.noreply.github.com>
2023-09-12 19:51:56 -0600

    Delete landing directory

--- landing/main.js
+++
@@ -1,8 +0,0 @@
-if('serviceWorker' in navigator){
-	console.log("Puedes usar los serviceWorker");
-	navigator.serviceWorker.register('./sw.js')
-	.then(res=>console.log("serviceWorker cargado correctamente",res))
-	.catch(err=>console.log("serviceWorker no se ha podido registrar",err))
-}else{
-	console.log("NO PUEDES usar los SercicesWordker");
-}
